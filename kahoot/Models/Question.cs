5cda74810828a8f448d90ecd484e534140a64b25
stenarvid <arre.holm@gmail.com>
2026-02-20 14:18:46 +0100

    :(

--- kahoot/Models/Question.cs
+++ kahoot/Models/Question.cs
@@ -4,7 +4,7 @@ namespace Models.Questions
     public class Question(string _id, string _text, List<string> _choices, int _correctAnswer)
     {
         public string ID = _id;
-        public string Text = _id;
+        public string Text = _text;
         public List<string> QuizChoices = _choices;
         public int CorrectAnswer;
     }
