15ec7e5611b43990a8150e27ab944e89c2878066
Coolastelammetcodes <Martinlam36@gmail.com>
2026-02-20 10:59:48 +0100

    Participant, filled class with properties

--- kahoot/Models/Participant.cs
+++ kahoot/Models/Participant.cs
@@ -3,5 +3,7 @@ namespace Models.Participant;
 
 public class Participant
 {
-
+   public int AnswerIndex { get; set; }
+   public string ParticipantID { get; set; }
+   public DateTime TimeStamp { get; set; }
 }
