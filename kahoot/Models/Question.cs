db8be37dece719099f411427a3a45690c4a1e132
stenarvid <arre.holm@gmail.com>
2026-02-20 10:55:57 +0100

    Questions.cs Done

--- kahoot/Models/Question.cs
+++ kahoot/Models/Question.cs
@@ -3,5 +3,10 @@ namespace Models.Question;
 
 public class Question
 {
-
+    string ID;
+    string Text;
+    Dictionary<string, int> Choices;
+    Dictionary<int, string> Answers;
+    int CorrectAnswer;
+    int[] VoteCount;
 }
